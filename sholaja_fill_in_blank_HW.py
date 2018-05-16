d3839676da912a5290c8f5ed1ee1dc03554fcb90
Mohammed S <37146251+MoShola312@users.noreply.github.com>
2018-05-16 08:38:27 -0500

    Update sholaja_fill_in_blank_HW.py

--- sholaja_fill_in_blank_HW.py
+++ sholaja_fill_in_blank_HW.py
@@ -1,35 +1,24 @@
+
 # -*- coding: cp1252 -*-
 medium_string = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ 
-
 takes by adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ 
-
     if you don't specify the value to return. ___2___ can be standard data types such as string, number, 
-
 dictionary,tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
+medium_answers = ["function", "arguements", "None", "list"]
 
 easy_string = '''A common first thing to do in programming is to display Hello ___1___! In ___2___ this is particulary easy; all you have to do is 
 type in: ___3___  “Hello ___1___”  Of course, that isn’t a very useful thing to do. However, it is an example of how output to the user using the ___3___ command, and
 produces a program which does something, so it is useful in that capacity.
 It may seem a bit odd to do something in a totally complete language that can be done even more easily with an ___4___ file in a browser, but it’s a step in learning ___2___ syntax,
 and that’s really its purpose.'''
+easy_answers = ["World", "python", "print", "py"]
 
 hard_string = '''Goku is the main hero of the manga series called ___1___. Goku is a ___2___ who lives on Earth.  He has two sons: ___3___ and ___4___. '''
-
+hard_answers = ["Dragonball", "Sayian", "Gohan", "Goten"]
 
 #List of blanks that will be passed in.
 blanks1 = ["___1___", "___2___","___3___", "___4___"]
 
-#checks for correct answers
-def hard_correct(users_selection):
-    for i in users_selection:
-        if "___1___" != "Dragonball":
-	    print "Incorrect, try agian"
-	if "___2___" != "Sayian":
-	    print "Incorrect, try agian"
-	if "___3___" != "Gohan":
-            print "Incorrect, try agian"
-	if "___4___" != "Goten":
-            print "Incorrect, try agian"
 
 
 #checks to see if strings in blanks1 are also in answer
@@ -73,13 +62,10 @@ while user_input2 != "easy" or "medium" or "hard":
             Possible choices include easy, medium, and hard. ''')
     if user_input2 == "easy":
         print play_game(easy_string, blanks1)
-    if user_input2 == "medium":
+    elif user_input2 == "medium":
         print play_game(medium_string, blanks1)
-    if user_input2 == "hard":
+    elif user_input2 == "hard":
         print play_game(hard_string, blanks1)
     
-        
-       
-        
-    
+
     
